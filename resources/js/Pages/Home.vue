c918aa1292448dda98fb53dc99c60e85d854a2ef
Thibeault Chenu <contact@tchenu.com>
2022-07-08 10:47:35 +0200

    Add margin bottom to home container

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -44,7 +44,7 @@ const getCarousels = async () => {
 <template>
     <AppLayout @city-changed="handleCityChanged" title="💃 Tickets for Live Music Events">
 
-        <div v-if="state.carousels" class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div v-if="state.carousels" class="max-w-6xl mx-auto sm:px-6 lg:px-8 mb-8">
             <div v-bind:key="index" v-for="(carousel, index) in state.carousels" class="
           mt-12
           container
